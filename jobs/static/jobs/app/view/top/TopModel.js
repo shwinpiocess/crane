b7de0a1e552dd614829fafe61a54fd0e02455900
shwinpiocess <shwinpiocess@gmail.com>
2015-04-23 12:18:42 +0800

    remove iconUrl

--- jobs/static/jobs/app/view/top/TopModel.js
+++ jobs/static/jobs/app/view/top/TopModel.js
@@ -10,7 +10,6 @@ Ext.define('Jobs.view.top.TopModel', {
         system: {
             name: '运维系统',
             version: '1.0.0',
-            iconUrl: '/static/jobs/resources/images/logo.gif'
         }
     }
 });
